b893120196aa250bef69f57ab49d3ba11115381b
Ricky Zhu <riozhu@gmail.com>
2021-09-24 11:37:44 -0700

    edit ticket

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -9,7 +9,15 @@ export class Home extends Component {
   
   constructor(props) {
     super(props);
-    this.state = { tickets: []};
+    this.state = { 
+      tickets: [], 
+      
+      id: '',
+      summary: '',
+      description:'',
+      isResolved: false,
+      creator: ''
+    };
   }
 
   componentDidMount() {
@@ -39,8 +47,36 @@ export class Home extends Component {
         .then(() => this.setState({ tickets: this.removeTicketById(id)}));
   }
 
-  handleEditTicket = (e) => {
-    console.log("Edit");
+  handleEditTicket = (id, summary, description, isResolved, creator) => {
+    // console.log("Edit");
+    // console.log(id);
+
+    // console.log("h53");
+    // console.log(id + summary + description + isResolved + creator);
+    let updatedTicket = {
+      id: id,
+      summary: summary,
+      description, description,
+      isResolved, isResolved,
+      creator: creator
+    }
+    // console.log("h62");
+    // console.log(updatedTicket);
+
+    // this.setState({id: id, summary: summary, description: description, isResolved: isResolved, creator: creator})
+    let newState = {... this.state};
+    newState.id = id;
+    newState.summary = summary;
+    newState.description = description;
+    newState.creator = creator;
+
+    this.setState(newState, () => {
+      // console.log("h68");
+      // console.log(newState);
+      // console.log(this.state);
+    })
+    
+    
   }
 
   generatePayloadAfterResolve = (id) => {
@@ -90,6 +126,70 @@ export class Home extends Component {
     });
   }
 
+  generateTicketsAfterEdit = (ticket, id) => {
+    // console.log('H128');
+    // console.log(newTicket);
+
+    let newState = {... this.state};
+    // update curTicket
+    newState.summary = ticket.summary;
+    newState.description = ticket.description;
+
+    // console.log("h137");
+    // console.log(newState);
+
+    // remove id ticket from newstate.tickets
+    newState.tickets = newState.tickets.filter((ele) => {
+      return ele.id !== id
+    })
+
+    
+    // append modified ticket to state.tickets
+    newState.tickets.push({
+      id: this.state.id,
+      summary: newState.summary,
+      description: newState.description,
+      creator: this.state.creator,
+      isResolved: this.state.isResolved
+    })
+
+    // console.log("h137");
+    // console.log(newState);
+
+    // update state
+    this.setState(newState, () => {
+      // console.log("h158");
+      // console.log(this.state);
+    });
+  }
+
+  handleCallBackEditTicket = (ticketData, id) => {
+    
+    let updatedTicket = {
+      id: this.state.id,
+      summary: this.state.summary,
+      description: this.state.description,
+      isResolved: this.state.isResolved,
+      creator: this.state.creator
+    };
+
+    updatedTicket.summary = ticketData.summary;
+    updatedTicket.description = ticketData.description;
+
+    // console.log("ticket after updated");
+    // console.log(updatedTicket);
+
+    fetch('api/tickets/' + id, { 
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTicket) 
+    }).then(() => {
+        this.generateTicketsAfterEdit(ticketData, id)
+      });
+  }
+
   renderTicketsTable(tickets) {
     // console.log(tickets);
 
@@ -113,7 +213,7 @@ export class Home extends Component {
               <td>{ticket.creator}</td>
               <td>{ticket.isResolved ? 'Yes' : 'No'}</td>
               <td><button onClick={() => {this.handleResolveTicket(ticket.id)}}>{ticket.isResolved ? 'Unresolve' : 'Resolve'}</button></td>
-              <td><button onClick={this.handleEditTicket}>Edit</button></td>
+              <td><button onClick={() => {this.handleEditTicket(ticket.id, ticket.summary, ticket.description, ticket.isResolved, ticket.creator)}}>Edit</button></td>
               <td><button onClick={() => {this.handleDeleteTicket(ticket.id)}}>Del</button></td>
             </tr>
           )}
@@ -128,10 +228,14 @@ export class Home extends Component {
     return (
       <div>
         <h1>Hello, List all tickets</h1>
-
+        {/* create form */}
         <TicketForm parentCallback={this.handleCallBackCreateTicket}></TicketForm>
 
         {tableContent}
+
+        {/* edit form */}
+        <TicketForm parentCallback={this.handleCallBackEditTicket} key={this.state.id} id={this.state.id} summary={this.state.summary} description={this.state.description}></TicketForm>
+
       </div>
     );
   }
--- ClientApp/src/components/TicketForm.js
+++ ClientApp/src/components/TicketForm.js
@@ -6,7 +6,12 @@ export class TicketForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { summary: '',  description: ''};
+    // console.log("123");
+    // console.log(this.props.summary);
+
+    this.state = { summary: this.props.summary,  description: this.props.description};
+    // console.log("123");
+    // console.log(this.state);
   }
 
   handleInputChange = (event) => {
@@ -17,31 +22,55 @@ export class TicketForm extends Component {
     this.setState({
       [name]: value
     });
+    // console.log('ticketForm');
     // console.log(this.state);
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    let ticket = {
-      ... this.state,
-      isResolve: false
-    }
+    let ticket;
+    if (this.props.summary == null || this.props.summary == undefined) {
+      // create ticket 
+      ticket = {
+        ... this.state,
+        isResolve: false
+      } 
+      this.props.parentCallback(ticket);
+    } else {
+      // update ticket
+      ticket = {... this.state}
 
-    this.props.parentCallback(ticket);
+      // console.log("TF40");
+      // console.log(ticket);
+      
+      this.props.parentCallback(ticket, this.props.id);
+    }
+    
   }
 
   render() {
+    // console.log(this.props);
     return (
       <div>
         <form>
           <FormGroup>
             <Label for="summary">Summary</Label>
-            <Input type="text" name="summary" id="summary" onChange={this.handleInputChange} />
+            {
+              this.props.summary != null 
+              ? <Input type="text" name="summary" id="summary" onChange={this.handleInputChange} value={this.state.summary}/> 
+              : <Input type="text" name="summary" id="summary" onChange={this.handleInputChange} />
+            }
+            
           </FormGroup>
 
           <FormGroup>
             <Label for="description">Descrption</Label>
-            <Input type="textarea" name="description" id="description" onChange={this.handleInputChange} />
+            {
+              this.props.summary != null 
+              ? <Input type="textarea" name="description" id="description" onChange={this.handleInputChange} value={this.state.description}/>
+              : <Input type="textarea" name="description" id="description" onChange={this.handleInputChange}/>
+            }
+            
           </FormGroup>
 
           <Button onClick={this.handleSubmit}>Submit</Button>
